0d69ad06966be3a0a3316c48954da3a396b1e014
Ziyu Huang <ziyu4huang@gmail.com>
2016-11-26 14:26:29 +0800

    Playground Example

---
+++ swift/HelloLib/HelloLib/HelloLib.h
@@ -0,0 +1,19 @@
+//
+//  HelloLib.h
+//  HelloLib
+//
+//  Created by Ziyu Huang on 2016/10/20.
+//  Copyright © 2016年 Ziyu Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloLib.
+FOUNDATION_EXPORT double HelloLibVersionNumber;
+
+//! Project version string for HelloLib.
+FOUNDATION_EXPORT const unsigned char HelloLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloLib/PublicHeader.h>
+
+
